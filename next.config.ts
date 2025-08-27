31d2a7cfe2b05a5ea3c16609cf4caa6b42670a6f
idtechnology1999 <owolabiidowu@yahoo.com>
2025-08-27 12:13:39 +0100

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -11,3 +11,15 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+
+// git init
+// git add .
+// git commit -m "Initial commit"
+
+// # 4. set main branch and connect remote
+// git branch -M main
+// git remote add origin https://github.com/idtechnology1999/web.git
+
+// # 5. push
+// git push -u origin main
