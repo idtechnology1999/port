393ce5188a3af401f4f0c8fe8ecbb71b5909f7b5
idtechnology1999 <owolabiidowu@yahoo.com>
2025-08-27 11:56:01 +0100

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["londonbookpublishers.com"], // ✅ no https://
+  },
 };
 
 export default nextConfig;
